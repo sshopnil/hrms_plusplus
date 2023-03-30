609ec581ac42c4ec8583c59190104abd39cfbf88
mithunHalder7 <87649933+mithunHalder7@users.noreply.github.com>
2023-03-30 11:30:51 +0600

    Project_started

---
+++ react-admin/src/scenes/dashboard/index.jsx
@@ -0,0 +1,22 @@
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { mockTransactions } from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import Header from "../../components/Header";
+
+const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      
+    </Box>
+  );
+};
+
+export default Dashboard;
