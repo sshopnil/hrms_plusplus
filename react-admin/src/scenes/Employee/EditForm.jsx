45408edee040e426ad783334038c16e2046e869e
mithunHalder7 <87649933+mithunHalder7@users.noreply.github.com>
2023-03-31 21:25:35 +0600

    Changed Modal

---
+++ react-admin/src/scenes/Employee/EditForm.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const EmployeeEditForm = () => {
+  return (
+    <Box>
+      <Typography id="modal-modal-title" variant="h6" component="h2">
+        Text in a modal
+      </Typography>
+      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+      </Typography>
+    </Box>
+  );
+};
+
+export default EmployeeEditForm;
--- react-admin/src/scenes/Employee/index.jsx
+++ react-admin/src/scenes/Employee/index.jsx
@@ -9,26 +9,14 @@ import { useState } from "react";
 import { useFormik } from "formik";
 import { mockDataEmployee } from "../../data/mockData";
 import ModalAddEmployee from "./ModalAdd";
-
-
 import useMediaQuery from "@mui/material/useMediaQuery";
-
 import Container from "@mui/material/Container";
 import EmpInfoModal from "./components/EmpInfoModal";
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import EmployeeEditForm from "./EditForm";
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+
 
 const EmlpoyeeList = () => {
   const theme = useTheme();
@@ -50,6 +38,7 @@ const EmlpoyeeList = () => {
     // const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    console.log(params)
     return (
       <strong>
         <Button
@@ -67,14 +56,7 @@ const EmlpoyeeList = () => {
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Text in a modal
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
-          </Box>
+        <EmployeeEditForm/>
         </Modal>
       </strong>
     )
