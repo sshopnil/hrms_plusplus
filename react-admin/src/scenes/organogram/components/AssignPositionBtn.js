4b7a1481fe57a537eedecf402fcccdadb7b34a67
sshopnil <shopnil710@gmail.com>
2023-04-01 13:14:52 +0600

    assign position scene updated

--- react-admin/src/scenes/organogram/components/AssignPositionBtn.js
+++ react-admin/src/scenes/organogram/components/AssignPositionBtn.js
@@ -2,28 +2,81 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Box from '@mui/material/Box';
-import DataPos from '../all_data/Data_positions.json';
-import DialogBtn from './DialogBtn';
+import { useFormik } from 'formik';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import Paper, { PaperProps } from '@mui/material/Paper';
+import Draggable from 'react-draggable';
+import TextField from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import DataPost from '../all_data/Data_positions.json';
+
+function PaperComponent(props: PaperProps) {
+  return (
+    <Draggable
+      handle="#draggable-dialog-title"
+      cancel={'[class*="MuiDialogContent-root"]'}
+    >
+      <Paper {...props} />
+    </Draggable>
+  );
+}
+
 
 const buttons = [
-  <Button key="one" 
-  sx={
-    {
-        padding:"10px",
+  <Button key="one"
+    sx={
+      {
+        padding: "10px",
         fontSize: "18px",
-        borderRadius : "20px",
+        borderRadius: "20px",
         background: "#25316D",
+      }
     }
-  }
-  onClick={()=>
-{
-    DataPos.map((item)=> console.log(item.id, item.pos_name))
-    // {DialogBtn}
-}}
+    value="1"
+
   >পদ তৈরি</Button>,
 ];
 
+const schemaTypeSelectionHandle = (event) => {
+  // console.log('key: ', $(event.target).data('key'));
+  console.log('key: ', event.target.value);
+}
+
 export default function AssignEmployeeBtn() {
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const formik = useFormik(
+    {
+      initialValues: {
+        পদের_নাম: "",
+      },
+      onSubmit:(values)=>{
+        console.log(values);
+      }
+    }
+  );
+
+  const [post, setPost] = React.useState('');
+  const [dept, setDept] = React.useState('');
+
+  const handleChange1 = (event) => {
+    setPost(event.target.value);
+  };
+  const handleChange2 = (event) => {
+    setDept(event.target.value);
+  };
+
   return (
     <Box
       sx={{
@@ -33,12 +86,78 @@ export default function AssignEmployeeBtn() {
         },
       }}
     >
+
       <ButtonGroup
         orientation="vertical"
         aria-label="vertical outlined button group"
       >
-        {buttons}
+        <Button key="one"
+          sx={
+            {
+              padding: "10px",
+              fontSize: "18px",
+              borderRadius: "20px",
+              background: "#25316D",
+            }
+          }
+          value="1"
+          onClick={handleClickOpen}
+        >পদ তৈরি</Button>
       </ButtonGroup>
+
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperComponent={PaperComponent}
+        aria-labelledby="draggable-dialog-title"
+        sx={{
+          '& .MuiDialogContent-root': {
+            backgroundColor: "#99C4C8",
+          },
+        }}
+
+      >
+        <DialogContent>
+          <Box sx={{ minWidth: 120 }}>
+            <form onSubmit={formik.onSubmit}>
+
+            <FormControl fullWidth>
+              <InputLabel id="post_name">পদের নাম</InputLabel>
+              <Select
+                labelId="post_name"
+                id="post_name"
+                value={post}
+                label="পদের নাম"
+                onChange={handleChange1}
+                name= "পদের_নাম"
+              >
+                {DataPost.map((info)=> <MenuItem value={info.id}>{info.name}</MenuItem>)}
+              </Select>
+              </FormControl>
+
+            <FormControl fullWidth>
+              <InputLabel id="dept">বিভাগ</InputLabel>
+              <Select
+                labelId="dept"
+                id="dept"
+                value={dept}
+                label="বিভাগ"
+                onChange={handleChange2}
+                name= "বিভাগ"
+              >
+                {DataPost.map((info)=> <MenuItem value={info.department.id}>{info.department.name}</MenuItem>)}
+              </Select>
+
+              <ButtonGroup variant="contained" aria-label="button group" sx={{m : "10px"}}>
+              <Button variant="contained" color='success'>প্রয়োগ</Button>
+              <Button variant="contained" color='error' onClick={handleClose} sx={{ml:"auto"}}>বাতিল</Button>
+              </ButtonGroup>
+            </FormControl>
+            </form>
+          </Box>
+        </DialogContent>
+      </Dialog>
     </Box>
   );
 }
