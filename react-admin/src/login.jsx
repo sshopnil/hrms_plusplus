42a6dbedd246ae6dee66c0349207ba5d4f3100c3
mithunHalder7 <87649933+mithunHalder7@users.noreply.github.com>
2023-04-06 20:08:21 +0600

    Login_page_design 2.0

--- react-admin/src/login.jsx
+++ react-admin/src/login.jsx
@@ -10,7 +10,6 @@ import Organogram from "./scenes/organogram";
 import EmlpoyeeList from "./scenes/Employee";
 import { Routes, Route } from "react-router-dom";
 
-
 function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -30,7 +29,7 @@ function LoginPage() {
     // send the username and password to the server for authentication
 
     if (password === "1234") {
-      sessionStorage.setItem('usr_active', password);
+      sessionStorage.setItem("usr_active", password);
       setShowModal(true);
       setTimeout(() => setShowModal(false), 2000);
       setLoggedIn(true);
@@ -39,8 +38,8 @@ function LoginPage() {
     }
   }
 
-  let active_usr = sessionStorage['usr_active'];
-  if (active_usr == "1234" | loggedIn) {
+  let active_usr = sessionStorage["usr_active"];
+  if ((active_usr == "1234") | loggedIn) {
     return (
       <div className="app">
         <Sidebar />
@@ -67,7 +66,6 @@ function LoginPage() {
             <Route path="/dashboard" element={<Dashboard />} />
             {/* <Route path="/organogram" element={<Organogram />} /> */}
             {/* <Route path="/Employee" element={<EmlpoyeeList />} /> */}
-            
           </Routes>
         </main>
       </div>
@@ -78,7 +76,6 @@ function LoginPage() {
     <div className="app">
       <LoggedInSidebar />
       <main className="content">
-        
         <Box
           sx={{
             display: "flex",
@@ -86,7 +83,9 @@ function LoginPage() {
             justifyContent: "center",
           }}
         >
-          <Typography variant="h3">লগ-ইন</Typography>
+          <Typography variant="h3" sx={{ p: 5 }}>
+            লগ-ইন
+          </Typography>
         </Box>
         <Box
           sx={{
